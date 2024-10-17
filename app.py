e70f1c340f6aaf96c96f0196afa1ebc9dcc0c003
unknown <sahltosahl36@gmail.com>
2024-10-17 18:23:43 +0530

    Initial commit

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'sarginamvv@gmail.com'
-app.config['MAIL_PASSWORD'] = 'hjti vtfi pibn amfv'
+app.config['MAIL_USERNAME'] = 'your@gmail.com'
+app.config['MAIL_PASSWORD'] = 'your app password'
 mail = Mail(app)
 
 db = SQLAlchemy(app)
